b6fca14f86e87b7a3b4c21da44722b3308151d6c
aakrut <aakrutdabhi@gmail.com>
2023-01-10 19:46:32 +0530

    add error handling

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -3,11 +3,76 @@ pragma solidity ^0.8.9;
 
 contract MultiSig {
     address[] public owners;
+    uint public transactionCount;
     uint public required;
 
-
-    constructor(address[] memory _owners,uint _required) {
-        owners = _owners;
-        required = _required;
+    struct Transaction {
+        address destination;
+        uint value;
+        bool executed;
+        bytes data;
     }
+
+    mapping(uint => Transaction) public transactions;
+    mapping(uint => mapping(address => bool)) public confirmations;
+
+    function getConfirmationsCount(uint transactionId) public view returns(uint) {
+        uint count;
+        for(uint i = 0; i < owners.length; i++) {
+            if(confirmations[transactionId][owners[i]]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function isOwner(address _addr) public view returns(bool) {
+        for(uint i=0 ;i<owners.length ;i++) {
+            if(owners[i] == _addr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function confirmTransaction(uint transactionId) public  {
+        require(isOwner(msg.sender));
+        confirmations[transactionId][msg.sender] = true;
+        if(isConfirmed(transactionId)) {
+            executeTransaction(transactionId);
+        }
+    }
+
+    function addTransaction(address destination, uint value,bytes memory data) internal returns(uint) {
+        transactions[transactionCount] = Transaction(destination, value,false, data);
+        transactionCount += 1;
+        return transactionCount - 1;
+    }
+
+    function submitTransaction(address _destination,uint _value,bytes memory _data) external {
+        uint id = addTransaction(_destination,_value,_data);
+        confirmTransaction(id);
+    }
+
+    function isConfirmed(uint _transactionId) public view returns(bool){
+      return getConfirmationsCount(_transactionId) >= required;
+    }
+
+    function executeTransaction(uint _transactionId) public {
+        require(isConfirmed(_transactionId));
+        Transaction storage _tx = transactions[_transactionId];
+        (bool success,) = _tx.destination.call{value:_tx.value}(_tx.data);
+        require(success,"Failed to Execute Transaction");
+        _tx.executed = true;
+    }
+
+    constructor(address[] memory _owners, uint _confirmations) {
+        require(_owners.length > 0);
+        require(_confirmations > 0);
+        require(_confirmations <= _owners.length);
+        owners = _owners;
+        required = _confirmations;
+    }
+
+    receive() external payable {}
 }
--- test/MultiSigTest.ts
+++ test/MultiSigTest.ts
@@ -1,29 +1,60 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect ,assert} from "chai";
+import { expect, assert } from "chai";
 import { ethers } from "hardhat";
 
-describe('MultiSig',function () {
-  let contract : any;
+describe("MultiSig", function () {
+  let contract: any;
   let _required = 2;
-  let accounts:any;
+  let accounts: any;
+
+  let MultiSig: any;
 
   before(async () => {
-     accounts = await ethers.provider.listAccounts();
-    const MultiSig = await ethers.getContractFactory("MultiSig");
+    accounts = await ethers.provider.listAccounts();
+    MultiSig = await ethers.getContractFactory("MultiSig");
     contract = await MultiSig.deploy(accounts.slice(0, 3), _required);
     await contract.deployed();
-  })
+  });
 
   it("should set an array of owners", async () => {
     let firstOwner = await contract.callStatic.owners(0);
     let lastOwner = await contract.callStatic.owners(2);
     assert.equal(accounts[2], lastOwner);
     assert.equal(accounts[0], firstOwner);
-  })
+  });
 
-  it("should set the number of required confirmations", async () => {
-    const required = await contract.callStatic.required();
-    assert.equal(_required, required);
-  })
-})
+   it("should set the number of required confirmations", async function () {
+     const required = await contract.required();
+     assert.equal(_required, required.toNumber());
+   });
+  
+  
+  describe("for a multisig with no owners", () => {
+    it("should revert", async () => {
+      await expectThrow(MultiSig.deploy([], 1));
+    });
+  });
+
+  describe("for a multisig with no required confirmations", () => {
+    it("should revert", async () => {
+      await expectThrow(MultiSig.deploy(accounts.slice(0, 3), 0));
+    });
+  });
+
+  describe("for a multisig with more required confirmations than owners", () => {
+    it("should revert", async () => {
+      await expectThrow(MultiSig.deploy(accounts.slice(0, 3), 4));
+    });
+  });
+});
+
+
+
+async function expectThrow(promise:any) {
+  const errMsg = "Expected throw not received";
+  try {
+    await promise;
+  } catch (err) {
+    return;
+  }
+  assert(false, errMsg);
+}
