970d1c8f06958f555427aba798e3ead496229892
aakrut <aakrutdabhi@gmail.com>
2023-01-11 22:39:16 +0530

    add script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      accounts:[PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
