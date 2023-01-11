8498889f73447ad2e7a21a291f96c85de52741e0
aakrut <aakrutdabhi@gmail.com>
2023-01-11 19:35:24 +0530

    add confirm transaction

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -24,10 +24,35 @@ contract MultiSig {
         required = _confirmations;
     }
 
+    function isOwner(address _addr) public view returns(bool) {
+        for(uint i=0 ;i<owners.length ;i++) {
+            if(owners[i] == _addr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     function addTransaction(address _destination,uint _value) public  returns(uint) {
         transactions[transactionCount] =     Transaction(_destination,_value,false);
         transactionCount+=1;
         return transactionCount -1;
+    }
+
+    function confirmTransaction(uint _transactionId) public {
+        require(isOwner(msg.sender));
+        confirmations[_transactionId][msg.sender] = true;
+    }
+
+    function getConfirmationsCount(uint _transactionId) public view returns(uint) {
+        uint count;
+        for(uint i=0;i < owners.length;i++) {
+            if(confirmations[_transactionId][owners[i]]) {
+                count ++;
+            }
+        }
+        return count;
     }
 
     receive() external payable {}
--- test/MultiSigTest.ts
+++ test/MultiSigTest.ts
@@ -129,6 +129,33 @@ describe("MultiSig", function () {
     assert.deepEqual(confirmations.outputs.map((x: any) => x.type), ["bool"])
   });
 
+ describe("after creating the first transaction", function () {
+   beforeEach(async () => {
+     await contract.addTransaction(accounts[1], 100);
+     await contract.confirmTransaction(0);
+   });
+
+   it("should confirm the transaction", async function () {
+     let confirmed = await contract.callStatic.getConfirmationsCount(0);
+     assert.equal(confirmed, 1);
+   });
+
+   describe("after creating the second transaction", function () {
+     beforeEach(async () => {
+       await contract.addTransaction(accounts[1], 100);
+       await contract.confirmTransaction(1);
+       await contract
+         .connect(ethers.provider.getSigner(accounts[1]))
+         .confirmTransaction(1);
+     });
+
+     it("should confirm the transaction twice", async function () {
+       let confirmed = await contract.callStatic.getConfirmationsCount(1);
+       assert.equal(confirmed, 2);
+     });
+   });
+ });
+
 });
 
 
