12ec86fe5efafaa0a320888b9962ca9b666ba180
aakrut <aakrutdabhi@gmail.com>
2023-01-11 21:09:53 +0530

    add fallback

--- test/MultiSigTest.ts
+++ test/MultiSigTest.ts
@@ -126,6 +126,25 @@ describe("MultiSig", function () {
    });
  });
 
+  describe("Fallback Tests", function () {
+    beforeEach(async () => {
+      let _required = 2;
+      accounts = await ethers.provider.listAccounts();
+      const MultiSig = await ethers.getContractFactory("MultiSig");
+      contract = await MultiSig.deploy(accounts.slice(0, 3), _required);
+      await contract.deployed();
+    });
+
+    it("should accept funds", async function () {
+      const value = ethers.utils.parseEther("1");
+      await ethers.provider
+        .getSigner(accounts[1])
+        .sendTransaction({ to: contract.address, value });
+      const balance = await ethers.provider.getBalance(contract.address);
+      assert.equal(balance.toString(), value.toString());
+    });
+  });
+
 });
 
 
